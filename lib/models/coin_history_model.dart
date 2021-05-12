b267db147d49a30d880e3c4233872cff5931067a
Nicholas Jensen <nickjensen098@gmail.com>
2021-05-12 18:49:20 +1000

    Create coin models

---
+++ lib/models/coin_history_model.dart
@@ -0,0 +1,9 @@
+class CoinHistory {
+  final int marketCap;
+  final double currentPrice;
+
+  CoinHistory({
+    this.marketCap,
+    this.currentPrice,
+  });
+}
---
+++ lib/models/coin_model.dart
@@ -0,0 +1,23 @@
+class Coin {
+  final String id;
+  final String symbol;
+  final String name;
+  final String imageUrl;
+  final int currentPrice;
+  final int marketCap;
+  final int marketCapRank;
+  final int highPrice;
+  final int lowPrice;
+
+  Coin({
+    this.id,
+    this.symbol,
+    this.name,
+    this.imageUrl,
+    this.currentPrice,
+    this.marketCap,
+    this.marketCapRank,
+    this.highPrice,
+    this.lowPrice,
+  });
+}
