db2c4c88e49a8d92a995f8578301e94f09deadc4
Nicholas Jensen <nickjensen098@gmail.com>
2021-05-14 17:49:56 +1000

    Remove starter code

---
+++ lib/screens/crypto_wallet_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CryptoWallet extends StatelessWidget {
+  const CryptoWallet({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
