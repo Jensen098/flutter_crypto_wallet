509b554fb1ae52b79b6f8971d1204b1323703e0f
Nicholas Jensen <nickjensen098@gmail.com>
2021-05-14 17:59:53 +1000

    Add custom colors

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColorDark = Color(0xFF091E24);
+const kPrimaryColorLight = Color(0xFF305C6A);
+const kSecondaryColor = Color(0xFFFF8915);
+const kAccentColorDeepBlue = Color(0xFF2E8AA6);
+const kAccentColorLightBlue = Color(0xFF99CBDA);
+const kAccentColorCyanBlue = Color(0xFF42F4FF);
